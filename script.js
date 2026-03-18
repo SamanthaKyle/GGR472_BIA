197597a54f122428d870cd1038061caaaa62f786
emmettzyoung <emmett.zyoung@gmail.com>
2026-03-18 19:22:06 -0400

    Updated some comments

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ GGR472 BIA: Non-Mapbox JS code
 /*--------------------------------------------------------------------
 ADDING INTERACTIVITY TO CARDS
 --------------------------------------------------------------------*/
-
+// Set the hovered status variables to null, but keep them as let to allow them to be changed
 let hoveredCard = null;
 let pinnedCard = null;
 let closeTimeout = null;
 
+// Create const varibles representing the scroll area and cards
 const cards = document.querySelectorAll('.mb-3[data-panel]');
 const scrollArea = document.querySelector('.scroll-area');
 
+// Create a for loop for mouse entering and clicking on a card
 cards.forEach(card => {
     card.addEventListener('mouseenter', () => {
         clearTimeout(closeTimeout);
